c0c23baa3a04aa00e62ca6dcfbed3d0548e63412
g3th <theinkandpen@yahoo.co.uk>
2024-06-05 05:34:09 +0100

    added: dynamic allocation, more complex excercise using pointers

--- mult.c
+++ mult.c
@@ -1,42 +1,28 @@
 #include <stdio.h>
-#define MAX_DATA 1024
+#include <stdlib.h>
 
 typedef struct {
-    double data[MAX_DATA];
-    int nrows;
-    int ncolumns;
+  double *data;
+  int nrows;
+  int ncols;
 } Matrix;
 
-void printMatrix(Matrix M){
-    int data_counter = 0;
-    printf("\n[");
-    for(int i=0; i<M.nrows;i++){
-        printf("\n");
-        for (int k=0; k<M.ncolumns; k++){
-            printf("%.3f  ", M.data[data_counter]);
-            data_counter++;
-        }
-    }
-    printf("\n]");
-}
-
-Matrix matrixMult(Matrix A, Matrix B)
+void matrixMult(Matrix *A, Matrix *B, Matrix *C)
 {   
-    Matrix C;
-    C.ncolumns = B.ncolumns;
-    C.nrows = A.nrows;
+    C->ncols = B->ncols;
+    C->nrows = A->nrows;
     int rows_done = 1;
     int temp=0;
     int data_a=0;
     int data_b=0;
     int index=0;
     double store_value[100];
-    while (rows_done <= A.nrows){
+    while (rows_done <= A->nrows){
         data_a=temp;
-        for (int i=0;i<A.nrows;i++){
-            for(int j=0; j<B.nrows;j++){
+        for (int i=0;i<A->nrows;i++){
+            for(int j=0; j<B->nrows;j++){
                 //printf("%.2f ", A.data[data_a] * B.data[i + data_b]);
-                store_value[index] = A.data[data_a] * B.data[i + data_b];
+                store_value[index] = A->data[data_a] * B->data[i + data_b];
                 data_a++;
                 index++;
                 data_b += 3;
@@ -49,30 +35,62 @@ Matrix matrixMult(Matrix A, Matrix B)
     }
     data_a = 0;
     index = 0;
-    for (int k=0;k<A.nrows;k++){
-        for (int j=0;j<B.ncolumns;j++){
+    for (int k=0;k<A->nrows;k++){
+        for (int j=0;j<B->ncols;j++){
             if (store_value[data_a]==0.0){
                     break;
             }
-            C.data[index] = store_value[data_a] + store_value[data_a+1];
+            C->data[index] = store_value[data_a] + store_value[data_a+1];
             data_a += 2;
             index++;
         }
     }
-  return C;
+}
+
+Matrix *createMatrix(int rows, int cols){
+  int matrix_size = rows * cols;
+  Matrix *m = (Matrix*)malloc(sizeof(Matrix));
+  m->data = (double*)malloc(sizeof(double) * matrix_size);
+  m->nrows = rows;
+  m->ncols = cols;
+  return m;
+}
+
+
+void printMatrix(Matrix *m){
+  int d = 0;
+  int rows = m->nrows;
+  int columns = m->ncols;
+  printf("[\n");
+  for (int j=0; j<m->nrows;j++){
+    for (int i=0; i<m->ncols;i++){
+      printf(" %.3f ",m->data[d]);
+      d++;
+    }
+    printf("\n"); 
+  }
+  printf("]\n\n");
 }
 
 int main(){
-    Matrix A = { {1.2, 2.3,
-                3.4, 4.5,
-                5.6, 6.7},
-                3,
-                2};
-    Matrix B = { {5.5, 6.6, 7.7,
-                1.2, 2.1, 3.3},
-                2,
-                3};
-    Matrix C = matrixMult(A, B);
-    printMatrix(C);
+  Matrix *A = createMatrix(3, 2);
+  A->data[0] = 1.2;
+  A->data[1] = 2.3;
+  A->data[2] = 3.4;
+  A->data[3] = 4.5;
+  A->data[4] = 5.6;
+  A->data[5] = 6.7;
+  printMatrix(A);
+  Matrix *B = createMatrix(2, 3);
+  B->data[0] = 5.5;
+  B->data[1] = 6.6;
+  B->data[2] = 7.7;
+  B->data[3] = 1.2;
+  B->data[4] = 2.1;
+  B->data[5] = 3.3;
+  printMatrix(B);
+  Matrix *C = createMatrix(3, 3);
+  matrixMult(A, B, C);
+  printMatrix(C);
   return 0;
 }
